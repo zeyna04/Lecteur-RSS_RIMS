68408720000823ac1c653807c7fb8fe791116b37
ramatou <ramakaffa@yahoo.fr>
2020-07-07 22:27:23 +0200

    Preparation du projet RSS Feed

---
+++ app/src/main/java/com/rama/rss_feed/MainActivity.java
@@ -0,0 +1,202 @@
+package com.rama.rss_feed;
+
+import android.os.AsyncTask;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private RecyclerView mRecyclerView;
+    private EditText mEditText;
+    private Button mFetchFeedButton;
+    private SwipeRefreshLayout mSwipeLayout;
+    private TextView mFeedTitleTextView;
+    private TextView mFeedLinkTextView;
+    private TextView mFeedDescriptionTextView;
+
+    private List<RssNewsModel> mFeedModelList;
+    private String mFeedTitle;
+    private String mFeedLink;
+    private String mFeedDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mEditText = findViewById(R.id.rssFeedEditText);
+        mFetchFeedButton = findViewById(R.id.fetchFeedButton);
+        mSwipeLayout = findViewById(R.id.swipeRefreshLayout);
+        mFeedTitleTextView = findViewById(R.id.feedTitle);
+        mFeedDescriptionTextView = findViewById(R.id.feedDescription);
+        mFeedLinkTextView = findViewById(R.id.feedLink);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mFetchFeedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FetchFeedTask().execute((Void) null);
+            }
+        });
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new FetchFeedTask().execute((Void) null);
+            }
+        });
+    }
+
+    public List<RssNewsModel> feedParsor(InputStream inputStream) throws XmlPullParserException, IOException {
+        String title = null;
+        String link = null;
+        String description = null;
+        boolean isItem = false;
+        List<RssNewsModel> items = new ArrayList<>();
+
+        try {
+            XmlPullParser xmlPullParser = Xml.newPullParser();
+            xmlPullParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            xmlPullParser.setInput(inputStream, null);
+
+            xmlPullParser.nextTag();
+            while (xmlPullParser.next() != XmlPullParser.END_DOCUMENT) {
+                int eventType = xmlPullParser.getEventType();
+
+                String name = xmlPullParser.getName();
+                if(name == null)
+                    continue;
+
+                if(eventType == XmlPullParser.END_TAG) {
+                    if(name.equalsIgnoreCase("item")) {
+                        isItem = false;
+                    }
+                    continue;
+                }
+
+                if (eventType == XmlPullParser.START_TAG) {
+                    if(name.equalsIgnoreCase("item")) {
+                        isItem = true;
+                        continue;
+                    }
+                }
+
+                Log.d("MainActivity", "Parsing name ==> " + name);
+                String result = "";
+                if (xmlPullParser.next() == XmlPullParser.TEXT) {
+                    result = xmlPullParser.getText();
+                    xmlPullParser.nextTag();
+                }
+
+                if (name.equalsIgnoreCase("title")) {
+                    title = result;
+                } else if (name.equalsIgnoreCase("link")) {
+                    link = result;
+                } else if (name.equalsIgnoreCase("description")) {
+                    description = result;
+                }
+
+                if (title != null && link != null && description != null) {
+                    if(isItem) {
+                        RssNewsModel item = new RssNewsModel(title, link, description);
+                        items.add(item);
+                    }
+                    else {
+                        mFeedTitle = title;
+                        mFeedLink = link;
+                        mFeedDescription = description;
+                    }
+
+                    title = null;
+                    link = null;
+                    description = null;
+                    isItem = false;
+                }
+            }
+
+            return items;
+        } finally {
+            inputStream.close();
+        }
+    }
+
+    private class FetchFeedTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String urlLink;
+
+        @Override
+        protected void onPreExecute() {
+            mSwipeLayout.setRefreshing(true);
+            mFeedTitle = null;
+            mFeedLink = null;
+            mFeedDescription = null;
+            mFeedTitleTextView.setText("Feed Title: " + mFeedTitle);
+            mFeedDescriptionTextView.setText("Feed Description: " + mFeedDescription);
+            mFeedLinkTextView.setText("Feed Link: " + mFeedLink);
+            urlLink = mEditText.getText().toString();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            if (TextUtils.isEmpty(urlLink))
+                return false;
+
+            try {
+                if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://"))
+                    urlLink = "http://" + urlLink;
+
+                URL url = new URL(urlLink);
+                InputStream inputStream = url.openConnection().getInputStream();
+                mFeedModelList = feedParsor(inputStream);
+                return true;
+            } catch (IOException e) {
+                Log.e(TAG, "Error", e);
+            } catch (XmlPullParserException e) {
+                Log.e(TAG, "Error", e);
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            mSwipeLayout.setRefreshing(false);
+
+            if (success) {
+                mFeedTitleTextView.setText("Feed Title: " + mFeedTitle);
+                mFeedDescriptionTextView.setText("Feed Description: " + mFeedDescription);
+                mFeedLinkTextView.setText("Feed Link: " + mFeedLink);
+                // Fill RecyclerView
+                mRecyclerView.setAdapter(new RssNewsListAdapter(mFeedModelList));
+            } else {
+                Toast.makeText(MainActivity.this,
+                        "Enter a valid Rss feed url",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
