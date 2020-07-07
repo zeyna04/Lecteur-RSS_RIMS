e44f1e020f5832086c62d4b424b9ee0225645eaf
ramatou <ramakaffa@yahoo.fr>
2020-07-07 23:10:54 +0200

    Le modèle de la Rss news fini

---
+++ app/src/main/java/com/rama/rss_feed/RssNewsModel.java
@@ -0,0 +1,13 @@
+package com.rama.rss_feed;
+
+public class RssNewsModel {
+    public String title;
+    public String link;
+    public String description;
+
+    public RssNewsModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
